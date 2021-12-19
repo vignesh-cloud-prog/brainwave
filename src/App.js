0bfa91807416cb9fd79ac7f4f2e480b5ba19a0a1
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-12-19 19:47:30 +0530

    site informations updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-// import logo from './logo.svg';
 import "./App.css";
 import axios from "axios";
 import React from "react";
 import { createApi } from "unsplash-js";
+import { Helmet } from "react-helmet";
+import favicon from "./favicon.ico"
 
 class App extends React.Component {
   state = {
     advice: "sky",
-    backgroundUrl:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW5zcGlyYXRpb258ZW58MHx8MHx8&w=1000&q=80",
+    backgroundUrl:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW5zcGlyYXRpb258ZW58MHx8MHx8&w=1000&q=80",
   };
 
   componentDidMount() {
     this.fetchQuote();
-    
   }
   componentDidUpdate(prevProps, prevState) {
     if (prevState.advice !== this.state.advice) {
-      console.log('pokemons state has changed.')
+      console.log("pokemons state has changed.");
       this.getImage();
       console.log(this.state.backgroundUrl);
     }
@@ -36,7 +37,7 @@ class App extends React.Component {
 
   getImage = () => {
     const unsplash = createApi({
-      accessKey: "5dowKUDsZplvMgjTzYin7ks0YLQKsp8XpO7_uHZov2s",
+      accessKey: process.env.REACT_APP_MY_ACCESS_KEY,
     });
 
     unsplash.search
@@ -44,7 +45,6 @@ class App extends React.Component {
         query: this.state.advice,
         page: 1,
         perPage: 1,
-        
       })
       .then((result) => {
         if (result.errors) {
@@ -54,17 +54,29 @@ class App extends React.Component {
           // handle success here
           const photo = result.response;
           // console.log(photo.results[0].urls.full);
-          const url=photo.results[0].urls.full
-          this.setState({backgroundUrl:url})
+          const url = photo.results[0].urls.full;
+          this.setState({ backgroundUrl: url });
         }
       });
-    
   };
   render() {
     return (
-      <div className="app" style={{ backgroundImage:`url(${this.state.backgroundUrl})` }} >
+      <div
+        className="app"
+        style={{ backgroundImage: `url(${this.state.backgroundUrl})` }}
+      >
+        <Helmet>
+          <title>Advizer</title>
+          <meta
+            name="description"
+            content="Get the most inspirational quotes at your finger tips. "
+          />
+          <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+        </Helmet>
         <div className="card">
+         
           <p className="heading">{this.state.advice}</p>
+          
           <button className="button" onClick={this.fetchQuote}>
             <span>GIVE ME ADVICE!</span>
           </button>
