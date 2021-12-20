bd6969eecf373801d5b41a446c30c4fc7b158c6f
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-12-20 23:28:00 +0530

    fontss changed

--- src/components/main.js
+++ src/components/main.js
@@ -39,19 +39,19 @@ export default class Main extends Component {
       });
   };
   fetchQuote = () => {
-    
     axios
       .get("https://api.adviceslip.com/advice")
       .then((response) => {
         const { advice } = response.data.slip;
         // console.log(advice);
-        this.setState({ advice },()=>{this.setBgImage();});
+        this.setState({ advice }, () => {
+          this.setBgImage();
+        });
       })
       .catch((error) => {
         console.log(error);
       });
   };
-
 
   render() {
     return (
@@ -67,9 +67,28 @@ export default class Main extends Component {
               content="Get the most inspirational quotes at your finger tips. "
             />
             <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap"
+              rel="stylesheet"
+            ></link>
+
+            <link
+              href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@500&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap"
+              rel="stylesheet"
+            />
           </Helmet>
           <div className="card">
-            <p className="heading">"{this.state.advice}"</p>
+            <b className="heading">"{this.state.advice}"</b>
 
             <button className="button" onClick={this.fetchQuote}>
               <span>ONE MORE ADVICE!</span>
